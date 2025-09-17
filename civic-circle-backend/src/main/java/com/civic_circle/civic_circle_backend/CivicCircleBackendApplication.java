dff1f6dbbad62de6d1f32b1a98adc1c92c435a86
Zaid Rakhange <zaidcommits.github@gmail.com>
2025-09-17 12:20:08 +0530

    initial sprintboot backend commit

---
+++ civic-circle-backend/src/main/java/com/civic_circle/civic_circle_backend/CivicCircleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.civic_circle.civic_circle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicCircleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicCircleBackendApplication.class, args);
+	}
+
+}
---
+++ civic-circle-backend/src/test/java/com/civic_circle/civic_circle_backend/CivicCircleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civic_circle.civic_circle_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicCircleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
